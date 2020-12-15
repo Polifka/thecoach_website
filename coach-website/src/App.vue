52d86028bf028aeaa79c27ad65ea05cc3d631f50
Jan Polifka <jpolifk1@smail.uni-koeln.de>
2020-12-15 17:08:01 +0100

    logo small

--- coach-website/src/App.vue
+++ coach-website/src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
-    <div class="row">
-      <div class="row margin-block-header">
-        <div class="row">
-            <img class="imgCss" src="./assets/logo.png" />
-            <h2>THEBUISNESSCOACH</h2>
-        </div>
-      </div>
-    </div>
-    <CoachNavbar />
+    <CoachHeader/>
+    <CoachNavbar/>
     <div class="main-container">
       <router-view></router-view>
     </div>
@@ -17,28 +10,22 @@
 
 <script>
 import CoachNavbar from "./components/CoachNavbar";
+import CoachHeader from "./components/CoachHeader";
+
 export default {
   name: "app",
   components: {
     CoachNavbar,
+    CoachHeader
   },
   props: {},
 };
 </script>
 
 <style scoped>
-.imgCss {
-  width: 40px;
-}
 .main-container {
   margin-left: 5%;
   margin-right: 5%;
   margin-top: 5px;
-}
-
-.margin-block-header {
-  margin: auto;
-  margin-top: 15px;
-  margin-bottom: 10px;
 }
 </style>
---
+++ coach-website/src/components/CoachHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+      <div class="row margin-block-header">
+        <div class="row">
+            <img class="imgCss" src="../assets/logo.png" />
+            <h2>THEBUISNESSCOACH</h2>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "CoachHeader",
+  props: {},
+};
+</script>
+
+<style scoped>
+.margin-block-header {
+  margin: auto;
+  margin-top: 15px;
+  margin-bottom: 10px;
+}
+
+.imgCss {
+  width: 40px;
+  height: 40px;
+}
+</style>

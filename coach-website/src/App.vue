f70568cfafe633e306314d1cd79e5800be4c3260
Jan Polifka <jpolifk1@smail.uni-koeln.de>
2020-12-15 11:19:37 +0100

    top coach header

--- coach-website/src/App.vue
+++ coach-website/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
-    <hello-world/>
+    <div class="row" style="margin-top:3px;">
+      <div class="col-1" style="padding-right:3px">
+        <img class="imgCss" src="./assets/logo.png" align="right"/>
+      </div>
+      <div class="col" style="padding-left:0px;">
+        <h2 class="headline" align="left">THEBUISNESSCOACH</h2>
+      </div>
+    </div>
+    <MainContainer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainContainer from './components/MainContainer.vue'
 export default {
-  components: { HelloWorld },
+  components: { MainContainer },
   name: 'app',
   props: {
   }
 }
 </script>
+
+<style scoped>
+.imgCss {
+  width: 40px;
+  margin-top: 5px;
+}
+
+.headline {
+  font-size: 40px;
+  font-weight: 1000;
+}
+</style>
---
+++ coach-website/src/components/MainContainer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <CoachNavbar />
+      <div
+        class="row jumbotron"
+        style="padding-bottom: 1rem; padding-top: 2rem"
+      >
+        <div class="col">
+          <h3>Implement social media</h3>
+        </div>
+        <div class="col">
+          <p class="text-container">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+        <div class="col">
+          <h3>Get in contact</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CoachNavbar from "../components/CoachNavbar";
+export default {
+  name: "mainContainer",
+  components: {
+    CoachNavbar,
+  },
+  props: {},
+};
+</script>
+
+<style scoped>
+
+.text-container {
+  background: white;
+  margin-top: 5px;
+}
+
+</style>
